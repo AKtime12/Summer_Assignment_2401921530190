bcbdff097b648debd6d0b994b44e660323626f42
AKtime12 <sarahul078@gmail.com>
2026-06-28 19:05:19 +0530

    added Week_3 Day_4 problems solution

---
+++ Week_3/Day_4/largestRectangleInHistogram.java
@@ -0,0 +1,20 @@
+class largestRectangleInHistogram {
+    public int largestRectangleArea(int[] heights) {
+         Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+        for (int i = 0; i <= n; i++) {
+            while (!stack.isEmpty() &&
+                  (i == n || heights[stack.peek()] >= heights[i])) {
+
+                int height = heights[stack.pop()];
+                int left = stack.isEmpty() ? -1 : stack.peek();
+
+                int width = i - left - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
