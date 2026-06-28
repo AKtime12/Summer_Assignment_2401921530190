3074bfe1b674e1c76eb83775984a65917154f633
AKtime12 <sarahul078@gmail.com>
2026-06-28 19:15:22 +0530

    added Week_3 Day_5 problems solution

---
+++ Week_3/Day_5/slidingWindowMax.java
@@ -0,0 +1,25 @@
+class slidingWindowMax {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            // Remove indices outside window
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            // Remove smaller elements
+            while (!deque.isEmpty() &&
+                    nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+
+            // Window formed
+            if (i >= k - 1) {
+                ans[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+        return ans;
+    }
+}
